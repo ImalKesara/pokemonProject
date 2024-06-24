39925008bad231494a3f1203ef9cc650a24847e9
LoneWolf <imalkesara2001@gmail.com>
2024-06-24 18:40:50 +0530

    category generations

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -7,9 +7,9 @@
 	console.log(data); // get data from api == +page.ts file
 </script>
 
-<div class="generations flex flex-row gap-1">
+<div class="generations flex flex-row gap-1 justify-center">
 	{#each generations as generation (generation.id)}
-		<div class="generation text-white font-bold justify-center m-2 py-2 px-2">
+		<div class="generation text-white font-bold justify-center m-2 py-2 px-2 border-2 rounded-lg">
 			{generation.main_region}
 		</div>
 	{/each}
@@ -18,7 +18,7 @@
 <div class="monsters flex flex-row flex-wrap justify-center">
 	{#each data.monsters as montser (monster.url)}
 		<div
-			class="monster rounded-lg bg-violet-200 relative flex flex-col justify-center items-center p-1 m-1 monster w-32 h-32 border-2 border-grey-200 hover:bg-violet-500 ease-in duration-150 cursor-pointer"
+			class="monster rounded-lg bg-violet-200 relative flex flex-col justify-center items-center p-1 m-1 monster w-32 h-32 border-2 border-grey-200 hover:bg-violet-300 ease-in duration-150 cursor-pointer"
 		>
 			<img src={montser.image} alt={montser.name} />
 			{montser.name}
