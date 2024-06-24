83a446659916120af59fd678eea5b25fee4650c8
LoneWolf <imalkesara2001@gmail.com>
2024-06-24 17:44:11 +0530

    center all items and inviduable cards center

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,13 +3,17 @@
 	import '../app.css';
 
 	export let data: PageData;
-	console.log(data); // get data from api == +page
+	console.log(data); // get data from api == +page.ts file
 </script>
 
-<div class="flex gap-1 flex-col">
+<div class="monsters flex flex-row flex-wrap justify-center">
 	{#each data.monsters as montser (monster.url)}
-		<div class=" border-t-2">
-			<p>{montser.id} {montser.name}</p>
+		<div
+			class="flex flex-col justify-center items-center p-1 m-1 monster w-32 h-32 border-2 border-red-500"
+		>
+			{montser.id}
+			{montser.name}
+			<img src={montser.image} alt={montser.name} />
 		</div>
 	{/each}
 </div>
