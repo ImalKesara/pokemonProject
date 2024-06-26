30f17fa75b1961fa48eb141415a881623b0aa9a0
LoneWolf <imalkesara2001@gmail.com>
2024-06-26 14:35:35 +0530

    Reactivity and Interaction

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,11 +1,22 @@
 <script lang="ts">
+	import { indexMonster } from './+page.ts';
 	import type { PageData } from './$types.js';
 	import '../app.css';
 	import { generations } from './generation.js';
 
-	export let data: PageData;
-	console.log(data); // get data from api == +page.ts file
+	export let data: PageData; // get data from api == +page.ts file
+
+	let monsterID: string;
+
+	$: monster = data.monsters.find((monster) => monster.id === monsterID);
+
+	const clickHanlder = (montser: indexMonster) => {
+		monsterID = montser.id;
+	};
 </script>
+
+<h1>{monsterID}</h1>
+<h1>{monster?.name}</h1>
 
 <div class="generations flex flex-row gap-1 justify-center">
 	{#each generations as generation (generation.id)}
@@ -16,9 +27,10 @@
 </div>
 
 <div class="monsters flex flex-row flex-wrap justify-center">
-	{#each data.monsters as montser (monster.url)}
+	{#each data.monsters as montser (montser.url)}
 		<div
 			class="monster rounded-lg bg-violet-200 relative flex flex-col justify-center items-center p-1 m-1 monster w-32 h-32 border-2 border-grey-200 hover:bg-violet-300 ease-in duration-150 cursor-pointer"
+			on:click={() => clickHanlder(montser)}
 		>
 			<img src={montser.image} alt={montser.name} />
 			{montser.name}
--- src/routes/+page.ts
+++ src/routes/+page.ts
@@ -1,14 +1,19 @@
 import type { PageLoad } from './$types';
+
+type apiMonster = {
+	name: string;
+	url: string;
+};
+
+export type indexMonster = apiMonster & {
+	id: string;
+	image: string;
+};
+
 export const load = (async ({ fetch }) => {
 	const respones = await fetch('https://pokeapi.co/api/v2/pokemon?limit=120');
 	const json = await respones.json();
-
-	type IndexMonster = {
-		name: string;
-		url: string;
-	};
-
-	const monsters = json.results.map((monster: IndexMonster) => {
+	const monsters: indexMonster[] = json.results.map((monster: IndexMonster) => {
 		const spilitUrl = monster.url.split('/');
 		const id = spilitUrl[spilitUrl.length - 2];
 		return {
